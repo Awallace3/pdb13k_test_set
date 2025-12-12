f2e0e0cbd47dac58f7f74069285f2a90d2f327d5
Austin Wallace <awallace43@gatech.edu>
2025-12-12 08:25:50 -0500

    sft cc

--- crystals/run.sh
+++ crystals/run.sh
@@ -1,2 +1,3 @@
 # python3 ./errors.py --APDC_plot --reference_analysis "CCSD(T)/CBS/CP"
-python3 ./main.py > crystal.log & disown
+# python3 ./main.py > crystal.log & di
+scancel 2676293 2676294 2674898 2673557 2673559 2673561 2673563
