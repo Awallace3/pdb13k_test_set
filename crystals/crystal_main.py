422353df51a55a459c2ee40980a8c5653d740d30
AMW Arch <awallace43@gatech.edu>
2025-12-13 08:28:25 -0500

    crystal

--- crystals/crystal_main.py
+++ crystals/crystal_main.py
@@ -3316,7 +3316,7 @@ def main():
     #     ap2_ap3_df_energies_des370k_tl(v='bm', N=tl_N)
     #     ap2_ap3_df_energies_des370k_tl(v='apprx', N=tl_N)
     # plot_crystal_lattice_energies_with_N(0, sft=False, tl_N=tl_N)
-    plot_crystal_lattice_energies_with_N(1, sft=False, tl_N=tl_N)
+    plot_crystal_lattice_energies_with_N(0, sft=False, tl_N=tl_N)
     # plot_crystal_lattice_energies_with_N(10, sft=False, tl_N=tl_N)
     # plot_crystal_lattice_energies_with_N(5, sft=False)
     return
---
+++ crystals/script.pml
@@ -0,0 +1,12 @@
+load dat.xyz
+
+color grey, elem c
+hide spheres,*
+show stick,*
+set_bond stick_radius, 0.2, v.
+set stick_h_scale,1
+zoom center,5
+center v.
+rotate x, 65
+set ray_opaque_background, off
+set opaque_background, off
